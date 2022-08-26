efc78342b75224c68f8936d29809339806a43cc3
David Yusta <david.yusta@guidesmiths.com>
2022-08-26 18:59:59 +0200

    refactor: refactor application schematics

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,96 +1,72 @@
-import { Rule, SchematicContext, externalSchematic, Tree, branchAndMerge, chain } from '@angular-devkit/schematics';
+import {Rule, SchematicContext, externalSchematic, Tree, branchAndMerge, chain} from '@angular-devkit/schematics';
+import {RunSchematicTask} from '@angular-devkit/schematics/tasks';
+import {join} from 'path';
+import {ApplicationSchema} from './application.schema';
 
-import { NodePackageInstallTask, RepositoryInitializerTask } from '@angular-devkit/schematics/tasks';
+interface PackageEntry {
+  name: string
+  value: string
+}
 
-// import { addPackageJsonDependency, NodeDependencyType } from 'schematics-utilities';
-// import {
-//   addPackageJsonDependency,
-//   // NodeDependency,
-//   NodeDependencyType,
-// } from '@schematics/angular/utility/dependencies';
+type PackageSection = 'scripts' | 'dependencies' | 'devDependencies';
 
-export function application(options: any): Rule {
+export function application(options: ApplicationSchema): Rule {
   return (tree: Tree, context: SchematicContext) => {
-
-    console.log('tree :>> ', options.path);
     return chain([
       branchAndMerge(createNestApplication(options)),
-
-
-      // Ejecutar git init
-
-      installPackageJsonDependencies(options.name)
+      addPackageToJson(options, {name: 'husky', value: '~8.0.1'}),
+      addNpmScript(options, {name: 'prepare', value: 'husky install'}),
+      initProject(options),
     ])(tree, context);
   }
 }
 
-function addPackageToPackageJson(host: Tree, name: string, pkg: string, version: string): Tree {
-  if (host.exists(`./${name}/package.json`)) {
-    const sourceText = host.read(`./${name}/package.json`)!.toString("utf-8");
-    const json = JSON.parse(sourceText) as any;
-    if (!json.dependencies) {
-      json.dependencies = {};
-    }
-    if (!json.dependencies[pkg]) {
-      json.dependencies[pkg] = version;
-      // json.dependencies = sortObjectByKeys(json.dependencies);
-    }
-
-
-    if (!json.scripts) {
-      json.scripts = {};
-    }
-    if (!json.scripts.prepare) {
-      const scriptName = "prepare";
-      const scriptValue= "husky install";
-      json.scripts[scriptName] = scriptValue;
-    }
-
-    host.overwrite(`./${name}/package.json`, JSON.stringify(json, null, 2));
-  }
-  return host;
+function createNestApplication(options: ApplicationSchema): Rule {
+  return externalSchematic(
+    '@nestjs/schematics',
+    'application',
+    {name: options.name},
+  );
 }
 
-function installPackageJsonDependencies(name: string): Rule {
+const sortObjectKeys = (o: { [x: string]: any; }): { [x: string]: any; } => (Object(o) !== o || Array.isArray(o)
+  ? o
+  : Object.keys(o).sort().reduce((a, k) => ({...a, [k]: sortObjectKeys(o[k])}), {}));
+
+function ensurePackageSection(tree: Tree, name: string, section: PackageSection, entry: PackageEntry, sort: boolean = false) {
+  const packageJsonPath = join(name, 'package.json');
+  if (tree.exists(packageJsonPath)) {
+    const sourceText = tree.read(packageJsonPath)!.toString('utf-8');
+    const json = JSON.parse(sourceText) as any;
+
+    if (!json[section]) {
+      json[section] = {};
+    }
+    json[section][entry.name] = entry.value;
+
+    if (sort) json[section] = sortObjectKeys(json[section]);
+
+    tree.overwrite(packageJsonPath, JSON.stringify(json, null, 2));
+  }
+  return tree;
+}
+
+function addNpmScript(options: ApplicationSchema, script: PackageEntry) {
+  return (host: Tree, _context: SchematicContext): Tree => {
+    return ensurePackageSection(host, options.name, 'scripts', script);
+  }
+}
+
+function addPackageToJson(options: ApplicationSchema, pkg: PackageEntry): Rule {
+  return (host: Tree, _context: SchematicContext): Tree => {
+    return ensurePackageSection(host, options.name, 'dependencies', pkg, true);
+  }
+}
+
+function initProject(options: ApplicationSchema): Rule {
   return (host: Tree, context: SchematicContext): Tree => {
-    // const dependency =  { type: NodeDependencyType.Default, version: '~6.1.1', name: '@angular/elements' }
-    const projectPath= `/${name}`
-    // addPackageJsonDependency(host, dependency);
-
-    addPackageToPackageJson(host, name, 'husky', '~8.0.1');
-
-
-    context.addTask(new RepositoryInitializerTask(projectPath, {}));
-
-
-    context.addTask(new NodePackageInstallTask(projectPath));
+    const taskId = context.addTask(new RunSchematicTask('git-init', options));
+    context.addTask(new RunSchematicTask('npm-install', options), [taskId]);
     return host;
   };
-}
-
-// function add(): Rule {
-//   return (host: Tree, context: SchematicContext) => {
-//     // The CLI inserts `@angular/cdk` into the `package.json` before this schematic runs. This
-//     // means that we do not need to insert the CDK into `package.json` files again. In some cases
-//     // though, it could happen that this schematic runs outside of the CLI `ng add` command, or
-//     // the CDK is only listed as a dev dependency. If that is the case, we insert a version based
-//     // on the current build version (substituted version placeholder).
-//     if (getPackageVersionFromPackageJson(host, '@angular/cdk') === null) {
-//       // In order to align the CDK version with other Angular dependencies that are setup by
-//       // `@schematics/angular`, we use tilde instead of caret. This is default for Angular
-//       // dependencies in new CLI projects.
-//       addPackageToPackageJson(host, '@angular/cdk', `~0.0.0-PLACEHOLDER`);
-
-//       // Add a task to run the package manager. This is necessary because we updated the
-//       // workspace "package.json" file and we want lock files to reflect the new version range.
-//       context.addTask(new NodePackageInstallTask());
-//     }
-//   };
-// }
-
-function createNestApplication(options: any): Rule {
-  // if (!options.initApp) {
-  //   return noop();
-  // }
-  return externalSchematic('@nestjs/schematics', 'application', options);
 }
