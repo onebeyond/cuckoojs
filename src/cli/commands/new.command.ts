541b88b6c51105c7c2b847cd598e8b0806682aec
David Yusta <david.yusta@guidesmiths.com>
2022-08-28 11:30:31 +0200

    feat: use git and npm runners on new command

--- src/cli/commands/new.command.ts
+++ src/cli/commands/new.command.ts
@@ -1,4 +1,6 @@
 import {SchematicRunner} from '../lib/runners/schematic.runner';
+import {GitRunner} from '../lib/runners/git.runner';
+import {NpmRunner} from '../lib/runners/npm.runner';
 
 export class NewCommand {
 	constructor(private readonly name: string) {}
@@ -6,6 +8,8 @@ export class NewCommand {
 	public async execute() {
 		try {
 			await this.generateNestApplication();
+			await this.initRepository();
+			await this.installPackages();
 		} catch (error: unknown) {
 			process.exit(1);
 		}
@@ -13,5 +17,13 @@ export class NewCommand {
 
 	private async generateNestApplication() {
 		return new SchematicRunner().generateNestApplication(this.name);
+	}
+
+	private async initRepository() {
+		return new GitRunner().init({folderName: this.name});
+	}
+
+	private async installPackages() {
+		return new NpmRunner().install(this.name);
 	}
 }
