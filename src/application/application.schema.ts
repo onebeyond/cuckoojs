1293b283d7269c48af0200874cd654a0c6afe537
David Yusta <david.yusta@guidesmiths.com>
2022-08-28 11:26:23 +0200

    refactor: remove schematics

--- src/application/application.schema.ts
+++
@@ -1,3 +0,0 @@
-export type ApplicationSchema = {
-	name: string;
-};
--- src/git-init/index.ts
+++
@@ -1,9 +0,0 @@
-import type {Rule, SchematicContext, Tree} from '@angular-devkit/schematics';
-import {execSync} from 'child_process';
-import {join} from 'path';
-
-export function gitInit(options: any): Rule {
-	return (_tree: Tree, _context: SchematicContext) => {
-		execSync(`git init --initial-branch main ${join(process.cwd(), options.name)}`);
-	};
-}
