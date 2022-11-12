39c3ff040fbc250787ab480a6b69c80deb8394ad
David Yusta <david.yusta@guidesmiths.com>
2022-11-12 14:04:38 +0100

    refactor: linter files

--- packages/@guidesmiths/cuckoojs-schematics/src/commitlint/commitlint.factory.ts
+++ packages/@guidesmiths/cuckoojs-schematics/src/commitlint/commitlint.factory.ts
@@ -1,49 +1,50 @@
+import type {
+	Rule,
+	SchematicContext,
+	Tree} from '@angular-devkit/schematics';
 import {
-  apply,
-  chain,
-  MergeStrategy,
-  mergeWith,
-  move,
-  Rule,
-  SchematicContext,
-  template,
-  Tree,
-  url
+	apply,
+	chain,
+	MergeStrategy,
+	mergeWith,
+	move,
+	template,
+	url,
 } from '@angular-devkit/schematics';
-import { normalize } from "@angular-devkit/core";
+import {normalize} from '@angular-devkit/core';
 
 export function main(options: any): Rule {
-  return (tree: Tree, context: SchematicContext) => {
-    context.logger.info('Adding commitlint ...');
-    const path = normalize(options.directory)
-    const templateSource = apply(url('./files'), [
-      template({}),
-      move(path)
-    ]);
+	return (tree: Tree, context: SchematicContext) => {
+		context.logger.info('Adding commitlint ...');
+		const path = normalize(options.directory);
+		const templateSource = apply(url('./files'), [
+			template({}),
+			move(path),
+		]);
 
-    const merged = mergeWith(templateSource, MergeStrategy.Overwrite)
+		const merged = mergeWith(templateSource, MergeStrategy.Overwrite);
 
-    return chain([
-      merged,
-      updatePackageJson(path)
-    ])(tree, context);
-  };
+		return chain([
+			merged,
+			updatePackageJson(path),
+		])(tree, context);
+	};
 }
 
 function updatePackageJson(directory: string): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    const path = `${directory}/package.json`;
-    const file = tree.read(path);
-    const json = JSON.parse(file!.toString());
+	return (tree: Tree, _context: SchematicContext) => {
+		const path = `${directory}/package.json`;
+		const file = tree.read(path) as unknown as string;
+		const json = JSON.parse(file.toString()) as Record<string, any>;
 
-    if (!json.devDependencies) {
-      json.devDependencies = {};
-    }
+		if (!json.devDependencies) {
+			json.devDependencies = {};
+		}
 
-    json.devDependencies['@commitlint/cli'] = '^17.1.2';
-    json.devDependencies['@commitlint/config-conventional'] = '^17.1.0';
+		json.devDependencies['@commitlint/cli'] = '^17.1.2';
+		json.devDependencies['@commitlint/config-conventional'] = '^17.1.0';
 
-    tree.overwrite(path, JSON.stringify(json, null, 2));
-    return tree;
-  }
+		tree.overwrite(path, JSON.stringify(json, null, 2));
+		return tree;
+	};
 }
