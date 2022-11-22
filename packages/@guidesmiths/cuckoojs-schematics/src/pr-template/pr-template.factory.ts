abf7b2bd6d3c6ce8d1181320e1de56e646f1f432
Daniel Alarcon <daniel.alarcon@onebeyond.com>
2022-11-22 15:18:04 +0100

    feat: improved file interpolation on pr-template

--- packages/@guidesmiths/cuckoojs-schematics/src/pr-template/pr-template.factory.ts
+++ packages/@guidesmiths/cuckoojs-schematics/src/pr-template/pr-template.factory.ts
@@ -1,22 +1,21 @@
+import type {
+	Rule,
+	SchematicContext,
+	Tree} from '@angular-devkit/schematics';
 import {
-  apply,
-  MergeStrategy,
-  mergeWith,
-  Rule,
-  SchematicContext,
-  template,
-  Tree,
-  url
+	apply,
+	MergeStrategy,
+	mergeWith,
+	template,
+	url,
 } from '@angular-devkit/schematics';
 
-export const main = (options: any): Rule => {
-  return (tree: Tree, context: SchematicContext) => {
-    context.logger.info(`Creating Pull Request Template`);
+export const main = (options: any): Rule => (tree: Tree, context: SchematicContext) => {
+	context.logger.info('Creating Pull Request Template');
 
-    const templateSource = apply(url(`./files/${options.gitProvider}`), [
-      template({...options})
-    ]);
+	const templateSource = apply(url('./files'), [
+		template({...options}),
+	]);
 
-    return mergeWith(templateSource, MergeStrategy.Overwrite)(tree, context);
-  };
-}
+	return mergeWith(templateSource, MergeStrategy.Overwrite)(tree, context);
+};
