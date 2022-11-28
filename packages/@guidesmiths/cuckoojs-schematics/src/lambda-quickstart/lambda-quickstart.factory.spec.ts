a2cda36f089f863d3bae5aad94f84de9eda6624d
Daniel Alarcon <daniel.alarcon@onebeyond.com>
2022-11-28 15:32:40 +0100

    chore: formatted test file tabs

--- packages/@guidesmiths/cuckoojs-schematics/src/lambda-quickstart/lambda-quickstart.factory.spec.ts
+++ packages/@guidesmiths/cuckoojs-schematics/src/lambda-quickstart/lambda-quickstart.factory.spec.ts
@@ -1,5 +1,5 @@
-import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {Tree} from '@angular-devkit/schematics';
+import {SchematicTestRunner} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
@@ -12,25 +12,25 @@ describe('lambda-quickstart', () => {
             .toPromise();
 
         expect(tree.files).toEqual([
-           "/some-folder/README.md",
-              "/some-folder/jest.config.js",
-              "/some-folder/package.json",
-               "/some-folder/serverless.yml",
-               "/some-folder/.nvmrc",
-              "/some-folder/.env.sample",
-               "/some-folder/docker/docker-compose.yml",
-               "/some-folder/docker/localstack/custom-config.sh",
-              "/some-folder/fixtures/helloFunction/regularRequest.json",
-               "/some-folder/src/components/container.js",
-              "/some-folder/src/components/config/config.js",
-               "/some-folder/src/components/controllers/helloController.js",
-             "/some-folder/src/components/controllers/tests/helloController.test.js",
-              "/some-folder/src/components/controllers/tests/mocks/logger.js",
-              "/some-folder/src/components/logger/logger.js",
-               "/some-folder/src/handlers/hello.js",
-               "/some-folder/src/handlers/schemas/helloInput.js",
-               "/some-folder/src/handlers/utils/extractPayload.js",
-               "/some-folder/src/handlers/utils/httpResponse.js"
+            "/some-folder/README.md",
+            "/some-folder/jest.config.js",
+            "/some-folder/package.json",
+            "/some-folder/serverless.yml",
+            "/some-folder/.nvmrc",
+            "/some-folder/.env.sample",
+            "/some-folder/docker/docker-compose.yml",
+            "/some-folder/docker/localstack/custom-config.sh",
+            "/some-folder/fixtures/helloFunction/regularRequest.json",
+            "/some-folder/src/components/container.js",
+            "/some-folder/src/components/config/config.js",
+            "/some-folder/src/components/controllers/helloController.js",
+            "/some-folder/src/components/controllers/tests/helloController.test.js",
+            "/some-folder/src/components/controllers/tests/mocks/logger.js",
+            "/some-folder/src/components/logger/logger.js",
+            "/some-folder/src/handlers/hello.js",
+            "/some-folder/src/handlers/schemas/helloInput.js",
+            "/some-folder/src/handlers/utils/extractPayload.js",
+            "/some-folder/src/handlers/utils/httpResponse.js"
         ]);
     });
 });
