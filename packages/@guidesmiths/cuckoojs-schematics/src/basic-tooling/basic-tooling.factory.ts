86f4da9c140b64872de615b8a0dc4fd0e19f5949
David Yusta <david.yusta@guidesmiths.com>
2023-11-10 21:20:16 +0100

    feat: execute nvmrc and nestjs-config schematics

--- packages/@guidesmiths/cuckoojs-schematics/src/basic-tooling/basic-tooling.factory.ts
+++ packages/@guidesmiths/cuckoojs-schematics/src/basic-tooling/basic-tooling.factory.ts
@@ -8,12 +8,13 @@ import {schematic} from '@angular-devkit/schematics';
 import {normalize} from '@angular-devkit/core';
 import {execSync} from 'child_process';
 import {resolve} from 'path';
+import {NodePackageInstallTask} from '@angular-devkit/schematics/tasks'
 
 export function main(options: {directory: string}): Rule {
-	return async (tree: Tree, context: SchematicContext) => {
-		context.logger.info('Adding Husky, Commitlint and Gitignore schematics');
+	return (tree: Tree, context: SchematicContext) => {
+		context.logger.info('Adding Husky, Commitlint, Gitignore, Nvmrc and NestJS-config...');
 
-		if (!tree.exists(normalize('package.json'))) {
+    if (!tree.exists(normalize(`${options.directory}/package.json`))) {
 			context.logger.warn(
 				'package.json file not found. Initializing package.json',
 			);
@@ -21,9 +22,19 @@ export function main(options: {directory: string}): Rule {
 		}
 
 		return chain([
+      schematic('nestjs-config', {...options, skipInstall: true}),
 			schematic('husky', options),
 			schematic('commitlint', options),
 			schematic('gitignore', options),
+			schematic('nvmrc', options),
+      installDependencies(),
 		]);
 	};
+}
+
+function installDependencies(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask());
+    return tree;
+  };
 }
