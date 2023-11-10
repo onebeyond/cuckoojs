bc57fdf83d565ff8be29f579f53d8f2201f8f613
David Yusta <david.yusta@guidesmiths.com>
2023-11-10 10:31:16 +0100

    refactor: add guard for missing default export in config files

--- packages/@guidesmiths/cuckoojs-schematics/src/nestjs-config/files/src/config/config.ts
+++ packages/@guidesmiths/cuckoojs-schematics/src/nestjs-config/files/src/config/config.ts
@@ -10,11 +10,12 @@ import { EEnvironment } from './dto/env.dto';
 async function loadConfigFiles(envName: string) {
   const envFile = join(__dirname, 'env', `${envName}.js`);
   const defaultFile = join(__dirname, 'env', 'default.js');
+
+  let defaultConfig;
+  let envConfig;
   try {
-    return {
-      default: (await import(defaultFile))?.default,
-      [envName]: (await import(envFile))?.default,
-    };
+    defaultConfig = await import(defaultFile);
+    envConfig = await import(envFile);
   } catch (error) {
     const missingFilename = /.*\/(?<filename>.*)\.ts'/.exec(error.message)
       ?.groups?.filename;
@@ -22,6 +23,15 @@ async function loadConfigFiles(envName: string) {
       `Config file for environment ${missingFilename} not found. Please, consider adding a ${missingFilename}.ts file to the "env" folder.`,
     );
   }
+
+  if (!defaultConfig.default || !envConfig.default)
+    throw new Error(
+      `Please, use "export default" syntax to export the configuration object.`,
+    );
+  return {
+    default: defaultConfig.default,
+    [envName]: envConfig.default,
+  };
 }
 
 export default async function (): Promise<ConfigDto> {
