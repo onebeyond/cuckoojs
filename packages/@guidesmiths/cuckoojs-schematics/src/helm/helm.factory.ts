5a9c071025dc0dc670af82f5566291b6031f7a61
Sergio Cámara <sergio.camara@guidesmiths.com>
2023-11-15 09:50:36 +0100

    fix: fixing conflicts

--- packages/@guidesmiths/cuckoojs-schematics/src/helm/helm.factory.ts
+++ packages/@guidesmiths/cuckoojs-schematics/src/helm/helm.factory.ts
@@ -1,36 +1,38 @@
 import {
-	type Rule,
-	type SchematicContext,
-	type Tree,
-} from '@angular-devkit/schematics';
-import {resolve} from 'path';
+  type Rule,
+  type SchematicContext,
+  type Tree,
+} from "@angular-devkit/schematics";
 
-import {RuleBuilderFactory} from './rule-builder/rule.builder.factory';
+import { RuleBuilderFactory } from "./rule-builder/rule.builder.factory";
 
 interface Options {
-	serviceName: string;
-	imageName: string;
-	resourcesLimitCpu: string;
-	resourcesLimitMemory: string;
-	resourcesRequestCpu: string;
-	resourcesRequestMemory: string;
-	autoscalingEnabled: boolean;
-	autoscalingReplicasMin: number;
-	autoscalingReplicasMax: number;
-	autoscalingTargetCpu: number;
+  serviceName: string;
+  imageName: string;
+  resourcesLimitCpu: string;
+  resourcesLimitMemory: string;
+  resourcesRequestCpu: string;
+  resourcesRequestMemory: string;
+  autoscalingEnabled: boolean;
+  autoscalingReplicasMin: number;
+  autoscalingReplicasMax: number;
+  autoscalingTargetCpu: number;
+  ingressController: string;
+  tlsCertManager: string;
+  domainName: string;
 }
 
 export function main(options: Options): Rule {
-	return (_tree: Tree, context: SchematicContext) => {
-		context.logger.info('Creating Helm Chart...');
+  return (_tree: Tree, context: SchematicContext) => {
+    context.logger.info("Creating Helm Chart...");
 
-		const ruleBuilder = new RuleBuilderFactory().create({
+    const ruleBuilder = new RuleBuilderFactory().create({
       ingressType: options.ingressController as string,
       tlsCertType: options.tlsCertManager as string,
       logger: context.logger,
     });
 
-    const rule = ruleBuilder.build(options, tree);
-    return rule(tree, context);
-	};
+    const rule = ruleBuilder.build(options, _tree);
+    return rule(_tree, context);
+  };
 }
